dba7a34d59d3aebd89bf33b90e51ce558c00e5f4
Kaique Oliveira <150181873+slkaique@users.noreply.github.com>
2025-11-12 21:32:59 -0300

    criado funcoes para operacoes no banco

---
+++ funcoes.py
@@ -0,0 +1,32 @@
+import sqlite3
+from datetime import datetime
+
+
+def conectar():
+    return sqlite3.connect("registro.db", check_same_thread=False)
+
+def criar_tabela():
+    conn = conectar()
+    conn.execute('''CREATE TABLE IF NOT EXISTS registros (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        descricao TEXT,
+                        tipo TEXT,
+                        valor REAL,
+                        desconto REAL,
+                        data TEXT
+                    )''')
+    conn.commit()
+    conn.close()
+
+def inserir_registro(descricao, tipo, valor, desconto):
+    conn = conectar()
+    conn.execute("INSERT INTO registros (descricao, tipo, valor, desconto, data) VALUES (?, ?, ?, ?, ?)",
+                 (descricao, tipo, valor, desconto, datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    conn.commit()
+    conn.close()
+
+def listar_registros():
+    conn = conectar()
+    df = conn.execute("SELECT * FROM registros").fetchall()
+    conn.close()
+    return df
